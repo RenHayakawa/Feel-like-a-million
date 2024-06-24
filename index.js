0857c30213cbe2d18884bf32124be950f5c850fa
lilibeth66 <lizi1056@gmail.com>
2024-06-25 00:17:47 +0300

    update feedback

--- index.js
+++ index.js
@@ -20,23 +20,19 @@ const slides = Array.from(slider.querySelectorAll('img'));
 const slideCount = slides.length;
 let slideIndex = 0;
 
-// Устанавливаем обработчики событий для кнопок
 prevButton.addEventListener('click', showPreviousSlide);
 nextButton.addEventListener('click', showNextSlide);
 
-// Функция для показа предыдущего слайда
 function showPreviousSlide() {
     slideIndex = (slideIndex - 1 + slideCount) % slideCount;
     updateSlider();
 }
 
-// Функция для показа следующего слайда
 function showNextSlide() {
     slideIndex = (slideIndex + 1) % slideCount;
     updateSlider();
 }
 
-// Функция для обновления отображения слайдера
 function updateSlider() {
     slides.forEach((slide, index) => {
         if (index === slideIndex) {
@@ -47,5 +43,4 @@ function updateSlider() {
     });
 }
 
-// Инициализация слайдера
 updateSlider();
